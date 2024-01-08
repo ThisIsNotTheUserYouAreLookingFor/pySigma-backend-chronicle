25d6b92f4ded51b19b6b74203142b4ff81aba400
pieter.vangoethem@delen.be <pieter.vangoethem@delen.be>
2024-01-08 05:33:12 +0000

    formatting

--- tests/test_chronicle_backend.py
+++ tests/test_chronicle_backend.py
@@ -3,16 +3,23 @@ from sigma.backends.chronicle import ChronicleBackend
 from sigma.collection import SigmaCollection
 from sigma.pipelines.chronicle import chronicle_windows_pipeline
 
+
 @pytest.fixture
 def chronicle_backend():
     return ChronicleBackend()
 
+
 @pytest.fixture
 def chronicle_custom_backend():
-    return ChronicleBackend(query_settings = lambda x: {"custom.query.key": x.title}, output_settings = {"custom.key": "customvalue"})
+    return ChronicleBackend(
+        query_settings=lambda x: {"custom.query.key": x.title},
+        output_settings={"custom.key": "customvalue"},
+    )
 
-def test_chronicle_and_expression(chronicle_backend : ChronicleBackend):
-    rule = SigmaCollection.from_yaml("""
+
+def test_chronicle_and_expression(chronicle_backend: ChronicleBackend):
+    rule = SigmaCollection.from_yaml(
+        """
             title: Test
             status: test
             logsource:
@@ -23,12 +30,15 @@ def test_chronicle_and_expression(chronicle_backend : ChronicleBackend):
                     fieldA: valueA
                     fieldB: valueB
                 condition: sel
-        """)
+        """
+    )
 
     assert chronicle_backend.convert(rule) == ['fieldA="valueA" AND fieldB="valueB"']
 
-def test_chronicle_or_expression(chronicle_backend : ChronicleBackend):
-    rule = SigmaCollection.from_yaml("""
+
+def test_chronicle_or_expression(chronicle_backend: ChronicleBackend):
+    rule = SigmaCollection.from_yaml(
+        """
             title: Test
             status: test
             logsource:
@@ -40,11 +50,14 @@ def test_chronicle_or_expression(chronicle_backend : ChronicleBackend):
                 sel2:
                     fieldB: valueB
                 condition: 1 of sel*
-        """)
+        """
+    )
     assert chronicle_backend.convert(rule) == ['fieldA="valueA" OR fieldB="valueB"']
 
-def test_chronicle_and_or_expression(chronicle_backend : ChronicleBackend):
-    rule = SigmaCollection.from_yaml("""
+
+def test_chronicle_and_or_expression(chronicle_backend: ChronicleBackend):
+    rule = SigmaCollection.from_yaml(
+        """
             title: Test
             status: test
             logsource:
@@ -59,11 +72,16 @@ def test_chronicle_and_or_expression(chronicle_backend : ChronicleBackend):
                         - valueB1
                         - valueB2
                 condition: sel
-        """)
-    assert chronicle_backend.convert(rule) == ['(fieldA="valueA1" OR fieldA="valueA2") AND (fieldB="valueB1" OR fieldB="valueB2")']
+        """
+    )
+    assert chronicle_backend.convert(rule) == [
+        '(fieldA="valueA1" OR fieldA="valueA2") AND (fieldB="valueB1" OR fieldB="valueB2")'
+    ]
 
-def test_chronicle_or_and_expression(chronicle_backend : ChronicleBackend):
-    rule = SigmaCollection.from_yaml("""
+
+def test_chronicle_or_and_expression(chronicle_backend: ChronicleBackend):
+    rule = SigmaCollection.from_yaml(
+        """
             title: Test
             status: test
             logsource:
@@ -77,6 +95,8 @@ def test_chronicle_or_and_expression(chronicle_backend : ChronicleBackend):
                     fieldA: valueA2
                     fieldB: valueB2
                 condition: 1 of sel*
-        """)
-    assert chronicle_backend.convert(rule) == ['(fieldA="valueA1" AND fieldB="valueB1") OR (fieldA="valueA2" AND fieldB="valueB2")']
-
+        """
+    )
+    assert chronicle_backend.convert(rule) == [
+        'fieldA="valueA1" AND fieldB="valueB1" OR fieldA="valueA2" AND fieldB="valueB2"'
+    ]
